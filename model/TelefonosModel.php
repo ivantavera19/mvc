17c34c3635b49f26e7fd9740c4b87e598f0a0bf5
ivantavera19 <ivan.tavera19@gmail.com>
2017-12-14 21:40:12 -0600

    Creacion de modelos vi102

--- model/TelefonosModel.php
+++ model/TelefonosModel.php
@@ -8,6 +8,19 @@ class TelefonosModel extends Model{
 		$pst->bindParam(":id", $id, PDO::PARAM_INT);
 		return $pst->fetchAll();
 	}
+
+	public function setTelefonos(int $id, string $telefono){
+		$pst = $this->db->prepare("insert into telefonos values (null, :id, :telefono)");
+		$pst->bindParam(":id", $id, PDO::PARAM_INT);
+		$pst->bindParam(":telefono", $telefono, PDO::PARAM_STR);
+		return $pst->execute();
+	}
+
+	public function deleteTelefonos(int $id){
+		$pst = $this->db->prepare("delete from telefonos where idContacto = :id")
+		$pst->bindParam(":id", $id, PDO::PARAM_INT);
+		return $pst->execute();
+	}
 }
 
 
